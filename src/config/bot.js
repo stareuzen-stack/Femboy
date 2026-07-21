2edd3f533e2714e23ac5c52f22db8ceddb3069c6
stareuzen-stack <juliuseuzen08@gmail.com>
2026-07-21 04:29:52 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -172,7 +172,7 @@ export const botConfig = {
     startingBalance: 0,
 
     // Maximum bank amount before upgrades (if upgrades are used).
-    baseBankCapacity: 100000,
+    baseBankCapacity: 1000000,
 
     // Daily reward amount.
     dailyAmount: 1000,
